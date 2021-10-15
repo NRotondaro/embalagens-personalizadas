ee66310f427270cf6f328dc5c70da472702551c0
NRotondaro <nikson_rotondaro@hotmail.com>
2021-10-15 19:56:47 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEnvelope,
+  faClipboardList,
+  faBroom,
+} from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
@@ -8,51 +13,51 @@ function App() {
       <header className='App-header'>
         <div className='classBox'>
           <a
-            className='App-link'
-            href='https://reactjs.org'
             target='_blank'
-            rel='noopener noreferrer'
+            rel='noreferrer'
+            href='https://drive.google.com/file/d/1ijUuJXJVJobFuvUt7nW0zkqjYtN0aSrF/view?usp=sharing'
           >
-            Catálogo
+            <h4 className='App-link' target='_blank' rel='noopener noreferrer'>
+              Catálogo
+            </h4>
+            <FontAwesomeIcon color='black' icon={faClipboardList} size='3x' />
           </a>
-          <FontAwesomeIcon icon={faEnvelope} />
-
         </div>
         <div className='classBox'>
           <a
-            className='App-link'
-            href='https://reactjs.org'
             target='_blank'
-            rel='noopener noreferrer'
+            rel='noreferrer'
+            href='mailto:contato@embalagenspersonalizadas.com'
           >
-            Email
+            <h4 className='App-link' target='_blank' rel='noopener noreferrer'>
+              Email
+            </h4>
+            <FontAwesomeIcon color='black' icon={faEnvelope} size='3x' />
           </a>
-          <FontAwesomeIcon icon={faEnvelope} />
-
         </div>
         <div className='classBox'>
           <a
-            className='App-link'
-            href='https://reactjs.org'
             target='_blank'
-            rel='noopener noreferrer'
+            rel='noreferrer'
+            href='https://wa.me/+5582988057662'
           >
-            WhatsApp
+            <h4 className='App-link' target='_blank' rel='noopener noreferrer'>
+              WhatsApp
+            </h4>
+            <FontAwesomeIcon color='black' icon={faWhatsapp} size='3x' />
           </a>
-          <FontAwesomeIcon icon={faEnvelope} />
-
         </div>
         <div className='classBox'>
           <a
-            className='App-link'
-            href='https://reactjs.org'
             target='_blank'
-            rel='noopener noreferrer'
+            rel='noreferrer'
+            href='https://drive.google.com/file/d/1eSugEmSkac5HjolPUaDwDUBFHFS1JULG/view?usp=sharing'
           >
-            Learn React
+            <h4 className='App-link' target='_blank' rel='noopener noreferrer'>
+              Identidade Visual
+            </h4>
+            <FontAwesomeIcon color='black' icon={faBroom} size='3x' />
           </a>
-          <FontAwesomeIcon icon={faEnvelope} />
-
         </div>
       </header>
     </div>

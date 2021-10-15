67ba913d6e6cce9c60bb118c26a7490d08a605bb
NRotondaro <nikson_rotondaro@hotmail.com>
2021-10-15 19:34:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='classBox'>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Catálogo
+          </a>
+          <FontAwesomeIcon icon={faEnvelope} />
+
+        </div>
+        <div className='classBox'>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Email
+          </a>
+          <FontAwesomeIcon icon={faEnvelope} />
+
+        </div>
+        <div className='classBox'>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            WhatsApp
+          </a>
+          <FontAwesomeIcon icon={faEnvelope} />
+
+        </div>
+        <div className='classBox'>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <FontAwesomeIcon icon={faEnvelope} />
+
+        </div>
       </header>
     </div>
   );
